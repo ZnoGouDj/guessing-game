9d02c100a5c9dde8715428aee6e1d770c165f13c
Egor <znogoud3@gmail.com>
2019-10-08 21:26:13 +0300

    feat: guessing game half-success solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
 class GuessingGame {
     constructor() {}
-
+    debugger;
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.floor(this.max/2) + Math.floor((this.min+1)/2);
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess()+1;
     }
 }
 
